08dfe015835ad4f89a03ec12d756fe2b274c7e77
andrewbantly <andrewbantly@gmail.com>
2023-05-03 15:30:07 -0700

    able to read user experiences

--- controllers/api-v1/experiences.js
+++ controllers/api-v1/experiences.js
@@ -1,2 +1,27 @@
 const router = require('express').Router();
-const db = require('../../models')
+const db = require('../../models');
+
+
+
+// GET /experiences 
+router.get('/', (req, res) => {
+        res.json({
+            msg: "Welcome to the experiences endpoint"
+        })
+
+})
+router.get('/:id', async (req, res) => {
+    try {
+        findUser = await db.User.findOne({
+            _id: req.body._id
+        })
+        res.json({
+            msg: findUser.Experiences
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+module.exports = router
